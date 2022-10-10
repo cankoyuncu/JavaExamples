6ce1903609ef047ccf2d656c47cdcb6320538fd4
cankoyuncu <2110206542@ogrenci.karabuk.edu.tr>
2022-10-10 18:17:47 +0300

    JavaExamples

--- loopDemo/src/Main.java
+++ loopDemo/src/Main.java
@@ -3,8 +3,9 @@ public class Main {
 
         //For Loop
 
-        for(int i=1;i<10;i++) {
-
+        for(int i=1;i<=10;i++) {
+            System.out.println(i);
         }
+        System.out.println("Dongu Bitti!");
     }
 }
